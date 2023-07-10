bf8038abec504db89a4d985c995ebd224318a773
Jeremy B <jeremy.baker@northwestern.edu>
2023-07-10 09:34:15 -0500

    Feature: Add logging to the LineReader

--- src/org/nlogo/langextension/LineReader.scala
+++ src/org/nlogo/langextension/LineReader.scala
@@ -14,12 +14,16 @@ class LineReader(in: InputStreamReader) extends Closeable with AutoCloseable {
   }
 
   def readLine(): String = {
+    Logger.current.logOne("LineReader.readLine()")
     response.clear()
     var readCount = in.read(buffer, 0, bufferSize)
     if (readCount != -1) { response.appendAll(buffer, 0, readCount) }
+    Logger.current.logMany("LineReader.readLine()") { Seq("first read", "readCount", readCount.toString, "response", response.toString) }
+
     while (readCount != -1 && response.last != '\n') {
       readCount = in.read(buffer, 0, bufferSize)
       if (readCount != -1) { response.appendAll(buffer, 0, readCount) }
+      Logger.current.logMany("LineReader.readLine()") { Seq("loop read", "readCount", readCount.toString, "response", response.toString) }
     }
 
     if (response.length == 0) {
