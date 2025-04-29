1c06c3e5aa6412af46425ad0b56af06e5c25c841
Isaac Brown <isaacbrown5763@gmail.com>
2025-04-29 12:37:25 -0500

    Updated to latest NetLogo version

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ Compile / scalaSource := baseDirectory.value / "src"
 resolvers += "netlogo" at "https://dl.cloudsmith.io/public/netlogo/netlogo/maven/"
 
 libraryDependencies ++= Seq(
-  "org.nlogo"          % "netlogo"        % "6.3.0"
+  "org.nlogo"          % "netlogo"        % "7.0.0-internal1"
 , "org.json4s"        %% "json4s-jackson" % "4.0.7"
 // not used by this library, but needed for NetLogo
 , "org.jogamp.jogl" % "jogl-all" % "2.4.0" from "https://jogamp.org/deployment/archive/rc/v2.4.0/jar/jogl-all.jar"
--- src/org/nlogo/langextension/config/ConfigEditor.scala
+++ src/org/nlogo/langextension/config/ConfigEditor.scala
@@ -1,10 +1,11 @@
 package org.nlogo.languagelibrary.config
 
 import java.awt.{ BorderLayout, GridBagLayout }
-import javax.swing.{ BorderFactory, JButton, JDialog, JFrame, JPanel }
+import java.awt.event.ActionEvent
+import javax.swing.{ AbstractAction, BorderFactory, JButton, JDialog, JFrame, JPanel }
 
 import org.nlogo.core.I18N
-import org.nlogo.swing.{ ButtonPanel, RichAction, RichJButton, Utils }
+import org.nlogo.swing.{ ButtonPanel, RichAction, Utils }
 
 class ConfigEditor(owner: JFrame, longName: String, extLangBin: String, config: Config, extraProperties: Seq[ConfigProperty]) extends JDialog(owner, longName) {
   private val runtimeMessage = s"Enter the path to your $extLangBin executable. If blank, then $longName will look for an appropriate version of $extLangBin to run on the system's PATH."
@@ -24,15 +25,17 @@ class ConfigEditor(owner: JFrame, longName: String, extLangBin: String, config: 
       prop.addToPanel(this, editPanel)
     })
 
-    val okButton = RichJButton(I18N.gui.get("common.buttons.ok")) {
-      save()
-      dispose()
-    }
+    val okButton = new JButton(new AbstractAction(I18N.gui.get("common.buttons.ok")) {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        save()
+        dispose()
+      }
+    })
     val cancelAction = RichAction(I18N.gui.get("common.buttons.cancel"))(_ => dispose())
-    val buttonPanel = ButtonPanel(
+    val buttonPanel = new ButtonPanel(Seq(
       okButton,
       new JButton(cancelAction)
-    )
+    ))
     getRootPane.setDefaultButton(okButton)
     Utils.addEscKeyAction(this, cancelAction)
 
